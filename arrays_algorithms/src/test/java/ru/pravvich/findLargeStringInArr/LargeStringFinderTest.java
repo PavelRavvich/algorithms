3bb952c063e6a4c81542ae153459e6ad52f10f11
Pavel <triodjangopiter@yandex.ru>
2017-08-19 21:02:42 +0300

    add finder largest length string in arr test

--- arrays_algorithms/src/test/java/ru/pravvich/findLargeStringInArr/LargeStringFinderTest.java
+++ arrays_algorithms/src/test/java/ru/pravvich/findLargeStringInArr/LargeStringFinderTest.java
@@ -1,5 +1,28 @@
+package ru.pravvich.findLargeStringInArr;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class LargeStringFinderTest {
 
+    private final LargeStringFinder finder = new LargeStringFinder();
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenArrIsEmptyThenThrowException() {
+        finder.findLargest(new String[]{});
+    }
+
+    @Test
+    public void whenArrContainOneElementThenReturnItElem() {
+        final String result = finder.findLargest(new String[]{"java"});
+        assertThat(result, is("java"));
+    }
+
+    @Test
+    public void whenArrContainTreeDifferentElemThenReturnLargest() {
+        final String res = finder.findLargest(new String[]{"a", "abc", "ab"});
+        assertThat(res, is("abc"));
+    }
 }

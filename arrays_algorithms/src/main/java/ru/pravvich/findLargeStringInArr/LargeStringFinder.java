bb2cfc3cb64b5c8ca14f9a21d70129185c1392f0
Pavel <triodjangopiter@yandex.ru>
2017-08-19 21:02:07 +0300

    add finder largest length string in arr

--- arrays_algorithms/src/main/java/ru/pravvich/findLargeStringInArr/LargeStringFinder.java
+++ arrays_algorithms/src/main/java/ru/pravvich/findLargeStringInArr/LargeStringFinder.java
@@ -1,4 +1,36 @@
 package ru.pravvich.findLargeStringInArr;
 
 public class LargeStringFinder {
+
+    public String findLargest(String[] src) {
+
+        if (src.length == 0) throw new IllegalArgumentException("Arr is empty");
+
+        if (src.length == 1) return src[0];
+
+
+        int currentLength = src[0].length();
+
+        int index = 0;
+
+        for (int i = 1; i != src.length; i++) {
+
+            if (src[i].length() > currentLength) {
+
+                index = i;
+
+                currentLength = src[i].length();
+
+            }
+        }
+
+        return src[index];
+    }
+
+    public static void main(String[] args) {
+
+        String[] arr = {"asd", "sd", "jhjkgh", "h"};
+
+        System.out.println(new LargeStringFinder().findLargest(arr));
+    }
 }

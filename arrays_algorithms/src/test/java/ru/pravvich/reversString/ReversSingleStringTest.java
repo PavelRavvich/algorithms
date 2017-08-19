28ea0accf00db3f8c83e156c4c843a25a6d0c39d
Pavel <triodjangopiter@yandex.ru>
2017-08-19 21:03:46 +0300

    add revers for single string test

--- arrays_algorithms/src/test/java/ru/pravvich/reversString/ReversSingleStringTest.java
+++ arrays_algorithms/src/test/java/ru/pravvich/reversString/ReversSingleStringTest.java
@@ -1,5 +1,33 @@
+package ru.pravvich.reversString;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class ReversSingleStringTest {
 
+    @Test
+    public void whenStringWith4CharsInThenReversedStringOut() {
+
+        final String result = new ReversSingleString("avaj").revers();
+
+        assertThat(result, is("java"));
+    }
+
+    @Test
+    public void whenStringWith2CharsInThenReversedStringOut() {
+
+        final String result = new ReversSingleString("ab").revers();
+
+        assertThat(result, is("ba"));
+    }
+
+    @Test
+    public void whenStringWith1CharInThenSameStringOut() {
+
+        final String result = new ReversSingleString("a").revers();
+
+        assertThat(result, is("a"));
+    }
 }

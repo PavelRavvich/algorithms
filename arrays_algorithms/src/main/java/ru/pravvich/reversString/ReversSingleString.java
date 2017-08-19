4767b2528c36133b92f70e592152e5f43a6c9451
Pavel <triodjangopiter@yandex.ru>
2017-08-19 21:03:22 +0300

    add revers for single string

--- arrays_algorithms/src/main/java/ru/pravvich/reversString/ReversSingleString.java
+++ arrays_algorithms/src/main/java/ru/pravvich/reversString/ReversSingleString.java
@@ -1,4 +1,43 @@
 package ru.pravvich.reversString;
 
+/**
+ * Task: revers string like 'abc' -> 'cba'.
+ * Solution: {@link ReversSingleString#revers()}.
+ */
 public class ReversSingleString {
+    /**
+     * Source string for reversing.
+     */
+    private String src;
+
+    /**
+     * Default constructor.
+     *
+     * @param src fir reversing.
+     */
+    public ReversSingleString(String src) {
+        this.src = src;
+    }
+
+    /**
+     * Revers string.
+     *
+     * @return reverted string.
+     */
+    public String revers() {
+
+        final char[] arrSrc = src.toCharArray();
+
+        for (int i = 0, j = arrSrc.length - 1; i < arrSrc.length / 2; i++, j--) {
+
+            char tmp = arrSrc[i];
+
+            arrSrc[i] = arrSrc[j];
+
+            arrSrc[j] = tmp;
+        }
+
+        return String.valueOf(arrSrc);
+    }
+
 }

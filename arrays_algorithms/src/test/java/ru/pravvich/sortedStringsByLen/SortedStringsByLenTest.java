687e902ecab844a4dbafa8c2380329c623ee52e3
Pavel <triodjangopiter@yandex.ru>
2017-08-20 13:21:49 +0300

    add test for sorting

--- arrays_algorithms/src/test/java/ru/pravvich/sortedStringsByLen/SortedStringsByLenTest.java
+++ arrays_algorithms/src/test/java/ru/pravvich/sortedStringsByLen/SortedStringsByLenTest.java
@@ -1,5 +1,18 @@
+package ru.pravvich.sortedStringsByLen;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class SortedStringsByLenTest {
+
+    @Test
+    public void whenSortingThenArrSortByStrLen() {
+        final String[] result = new SortedStringsByLen()
+                .sortByLengthBubble(new String[]{"12", "123", "1"});
+
+        assertThat(result, is(new String[]{"1", "12", "123"}));
+    }
 
 }

803ba67a653d5e8a32d9c191769e383c48f84293
Pavel <triodjangopiter@yandex.ru>
2017-08-20 14:14:12 +0300

    add heap sort queue test

--- traditional_sort_algotythms/src/test/java/heapSort/HeapSortClassicImplTest.java
+++ traditional_sort_algotythms/src/test/java/heapSort/HeapSortClassicImplTest.java
@@ -1,5 +1,19 @@
+package heapSort;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class HeapSortClassicImplTest {
 
+    @Test
+    public void heapSortTest() {
+
+        int[] arr = {123, 1, 12};
+
+        new HeapSortClassicImpl().heapSort(arr);
+
+        assertThat(arr, is(new int[]{1, 12, 123}));
+    }
 }
